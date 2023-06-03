28b4ed61601c5359c73e3ba30162eaf278db8cc2
JoaquinMB2002 <133446839+JoaquinMB2002@users.noreply.github.com>
2023-06-02 19:27:14 -0500

    creacion de carpeta model con dao terminado

---
+++ grupo KRICA CELL/t2/src/main/java/com/proyecto/t2/model/dao/IMarcaDAO.java
@@ -0,0 +1,9 @@
+package com.proyecto.t2.model.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.proyecto.t2.model.entidad.Marca;
+
+public interface IMarcaDAO extends CrudRepository<Marca,Long>{
+    
+}
