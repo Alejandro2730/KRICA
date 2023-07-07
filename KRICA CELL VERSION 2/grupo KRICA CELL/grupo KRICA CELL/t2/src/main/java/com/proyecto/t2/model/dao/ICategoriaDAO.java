6117341bc557e364ac10ad00fd403af51ff98164
JoaquinMB2002 <133446839+JoaquinMB2002@users.noreply.github.com>
2023-07-07 12:41:52 -0500

    Delete KRICA CELL VERSION 2/grupo KRICA CELL directory
    directorio duplicado

--- KRICA CELL VERSION 2/grupo KRICA CELL/grupo KRICA CELL/t2/src/main/java/com/proyecto/t2/model/dao/ICategoriaDAO.java
+++
@@ -1,8 +0,0 @@
-package com.proyecto.t2.model.dao;
-import org.springframework.data.repository.CrudRepository;
-
-import com.proyecto.t2.model.entidad.Categoria;
-
-public interface ICategoriaDAO extends CrudRepository<Categoria, Long>{
-    
-}
