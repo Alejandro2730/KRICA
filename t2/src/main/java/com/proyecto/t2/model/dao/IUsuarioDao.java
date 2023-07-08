6ba3c688d3b7b9a416df88aff958d69d305e51b7
CristianRojasMendieta <119373638+CristianRojasMendieta@users.noreply.github.com>
2023-07-07 22:57:04 -0500

    IUsuari_commit

---
+++ t2/src/main/java/com/proyecto/t2/model/dao/IUsuarioDao.java
@@ -0,0 +1,9 @@
+package com.proyecto.t2.model.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.proyecto.t2.model.entidad.Usuario;
+
+public interface IUsuarioDao extends CrudRepository<Usuario, Long>{
+    public Usuario findByUsername(String username); 
+}
